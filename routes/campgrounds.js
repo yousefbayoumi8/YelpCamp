c1d5996e86c545d81ad5ee21877c799c456cc2e1
yousefbayoumi8 <56967025+yousefbayoumi8@users.noreply.github.com>
2023-04-04 05:39:54 -0300

    Added basic Authentication

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const catchAsync = require('../utils/catchAsync')
 const { campgroundSchema } = require('../schemas')
+const { isLoggedIn } = require('../authMiddleware')
 
 const ExpressError = require('../utils/ExpressError')
 const Campground = require('../models/campground')
@@ -24,12 +25,13 @@ router.get(
     })
 )
 
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
     res.render('campgrounds/new')
 })
 
 router.post(
     '/',
+    isLoggedIn,
     validateCampground,
     catchAsync(async (req, res) => {
         const campground = new Campground(req.body.campground)
@@ -53,6 +55,7 @@ router.get(
 
 router.get(
     '/:id/edit',
+    isLoggedIn,
     catchAsync(async (req, res) => {
         const campground = await Campground.findById(req.params.id)
         if (!campground) {
@@ -65,6 +68,7 @@ router.get(
 
 router.put(
     '/:id',
+    isLoggedIn,
     validateCampground,
     catchAsync(async (req, res) => {
         const { id } = req.params
@@ -78,6 +82,7 @@ router.put(
 
 router.delete(
     '/:id',
+    isLoggedIn,
     catchAsync(async (req, res) => {
         const { id } = req.params
         await Campground.findByIdAndDelete(id)
